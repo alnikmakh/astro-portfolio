58ddc397148b22d07bdfd4a74732e50d2e61763b
alexandermakhorin <alnikmakh@gmail.com>
2024-03-19 19:07:55 +0100

    presentation as mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
 import robotsTxt from "astro-robots-txt";
 import { SITE_URL } from "./src/data/config";
 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), robotsTxt()],
+  integrations: [mdx(), tailwind(), sitemap(), robotsTxt()],
   site: SITE_URL,
 });
